83da4a5c9cbf865a950c6166510ddbe72f7f435f
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-01-26 19:27:03 +0200

    Part1: 1.7-10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,8 +1,50 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Positive = ({ good, all }) => {
+    const pos_ratio = good / all
+    if (isNaN(pos_ratio)) {
+        return (
+            <div>positive %</div>
+        )
+    }
+    return (
+        <div>positive {pos_ratio} %</div>
+    )
+}
+
+const Average = ({ good, bad, all }) => {
+    const avg = (good + bad * (-1)) / all
+    if (isNaN(avg)) {
+        return (
+            <div>average</div>
+        )
+    }
+    return (
+        <div>average {avg}</div>
+    )
+}
+
 const Total = ({ rating, value }) => {
     return (<div>{rating} {value}</div>)
+}
+
+const StatisticLine = ({text, value}) => <div>{text} {value}</div>
+
+const Statistics = ({ good, neutral, bad, all }) => {
+    if(all==0) {
+        return (<div>No feedback given</div>)
+    }
+    const pos = good/all
+    return (
+        <div>
+            <StatisticLine text="good" value ={good} />
+            <StatisticLine text="neutral" value ={neutral} />
+            <StatisticLine text="bad" value ={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={(good+bad*(-1))/all} />
+            <StatisticLine text="positive" value={[pos,"%"].join(" ")} />
+        </div>)
 }
 
 const Button = ({ text, handler }) => {
@@ -18,18 +60,22 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
 
     const handleGoodClick = () => {
         console.log('clicked good')
         setGood(good + 1)
+        setAll(all + 1)
     }
     const handleNeutralClick = () => {
         console.log('clicked neutral')
         setNeutral(neutral + 1)
+        setAll(all + 1)
     }
     const handleBadClick = () => {
         console.log('clicked bad')
         setBad(bad + 1)
+        setAll(all + 1)
     }
 
     return (
@@ -41,11 +87,7 @@ const App = () => {
                 <Button text="bad" handler={handleBadClick} />
             </div>
             <h2>statistics</h2>
-            <div>
-                <Total rating="good" value={good} />
-                <Total rating="neutral" value={neutral} />
-                <Total rating="bad" value={bad} />
-            </div>
+            <Statistics good={good} neutral={neutral} bad={bad} all={all} />
         </>
     )
 }
