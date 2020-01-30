8deff0a2b3b2d19ca1a7cae15ec5a0ce90b98ac6
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-01-30 16:15:33 +0200

    Part2: 2.6

---
+++ part2/puhelinluettelo/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Entry from './components/Entry'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    console.log(persons)
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <Entry key={person.name} name={person.name} number={0} />)}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
