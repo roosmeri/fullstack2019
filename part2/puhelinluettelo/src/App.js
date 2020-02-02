0b64d6a28e600fc1dfa47fa61a20fa2968678918
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-02 22:14:20 +0200

    Part 2: 2.11

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Entry from './components/Entry'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('got response on persons')
+      setPersons(response.data)
+    })
+  },[])
+
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.map(person => person.name).indexOf(newName) !== -1 ) {
+    if (persons.map(person => person.name).indexOf(newName) !== -1) {
       alert(`${newName} is already added to phonebook`)
       return
     }
@@ -44,9 +52,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
       <h3>Add a new number</h3>
-      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
       <div>
         {personsToShow.map(person => <Entry key={person.name} name={person.name} number={person.number} />)}
