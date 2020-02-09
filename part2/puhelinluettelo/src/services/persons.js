49c051bb14c58204e0891902666f5c17a06eeb2d
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-09 17:32:48 +0200

    3.11

--- part2/puhelinluettelo/src/services/persons.js
+++ part2/puhelinluettelo/src/services/persons.js
@@ -1,23 +1,26 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(`${baseUrl}`)
-  return request.then(response => response.data)
+  return request.then(response => {
+    console.log(response.data)
+    return response.data
+  })
 }
 
 const create = newObject => {
-  const request = axios.post(`${baseUrl}/api/persons`, newObject)
+  const request = axios.post(`${baseUrl}/`, newObject)
   return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
- const request = axios.put(`${baseUrl}/api/persons/${id}`, newObject)
- return request.then(response => response.data)
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
 }
 
 const deletion = (id) => {
-  const request = axios.delete(`${baseUrl}/api/persons/${id}`)
+  const request = axios.delete(`${baseUrl}/${id}`)
   return request.then(response => response.data)
 }
 
