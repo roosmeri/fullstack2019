41115b38ca130494547d265193c5422dd627a691
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-14 11:44:15 +0200

    4.4

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((a, b) => a + b.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
