b2a26c29084b1d317af09e0fa0a7bc6ae60f9689
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-17 14:05:26 +0200

    4.15

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,31 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response, next) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-        .catch(error => next(error))
+blogsRouter.get('/', async (request, response, next) => {
+    try {
+        const blogs = await Blog.find({})
+        response.json(blogs)
+    } catch (exception) {
+        next(exception)
+    }
 })
 
-blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => next(error))
-
+blogsRouter.post('/', async (request, response, next) => {
+    try {
+        const blog = await new Blog(request.body).save()
+        response.status(201).json(blog)
+    } catch (exception) {
+        next(exception)
+    }
 }
 )
 
 blogsRouter.delete('/:id', async (request, response, next) => {
     try {
-        const repo = await Blog.findByIdAndRemove(request.params.id)
+        const blog = await Blog.findByIdAndRemove(request.params.id)
         response.status(204).end()
     } catch (exception) {
         next(exception)
