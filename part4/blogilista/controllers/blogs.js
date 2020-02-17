868f8216ce38ac12af68be7c52ef4a1460f12926
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-17 15:06:59 +0200

    4.20-21

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -4,14 +4,6 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.get('/', async (request, response, next) => {
     try {
         const blogs = await Blog.find({})
@@ -24,9 +16,7 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
     try {
-        const token = getTokenFrom(request)
-
-        const decodedToken = jwt.verify(token, process.env.SECRET)
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
         if (!token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
         }
@@ -54,7 +44,21 @@ blogsRouter.post('/', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
     try {
-        const blog = await Blog.findByIdAndRemove(request.params.id)
+        const token = getTokenFrom(request)
+
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+        const user = await User.findById(decodedToken.id)
+        const blog = await Blog.findById(request.params.id)
+        console.log(user)
+        console.log(blog.user)
+        if (user._id === blog.user._id) {
+            await Blog.findByIdAndRemove(request.params.id)
+        } else {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
         response.status(204).end()
     } catch (exception) {
         next(exception)
--- part4/blogilista/utils/middleware.js
+++ part4/blogilista/utils/middleware.js
@@ -26,9 +26,19 @@ const errorHandler = (error, request, response, next) => {
 
     next(error)
   }
+}
 
-  module.exports = {
-    requestLogger,
-    unknownEndpoint,
-    errorHandler
-  }
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+  next()
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor
+}
