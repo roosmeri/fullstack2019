63776af6b07878fe4b2f07bc0282268e0a2ddc9d
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-02-17 13:42:33 +0200

    4.14

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -26,13 +26,28 @@ blogsRouter.post('/', (request, response, next) => {
 blogsRouter.delete('/:id', async (request, response, next) => {
     try {
         const repo = await Blog.findByIdAndRemove(request.params.id)
-
         response.status(204).end()
-
     } catch (exception) {
         next(exception)
     }
 }
 )
+
+blogsRouter.put('/:id', async (request, response, next) => {
+    const body = request.body
+
+    const blog = {
+        author: body.author,
+        title: body.title,
+        likes: body.likes,
+        url: body.url
+    }
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        response.json(updatedBlog.toJSON())
+    } catch (exception) {
+        next(exception)
+    }
+})
 
 module.exports = blogsRouter
