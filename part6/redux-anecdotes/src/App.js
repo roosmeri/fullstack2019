c38c14ba7073ceeb7ef7c377adb4a6fa91c57430
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-03-02 13:31:49 +0200

    6.3-6.4, 6.6-6.7

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  voteAnecdote
+} from './reducers/anecdoteReducer'
+
+import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(voteAnecdote(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <AnecdoteForm dispatch={dispatch}/>
+    </div>
+  )
+}
+
+export default App
