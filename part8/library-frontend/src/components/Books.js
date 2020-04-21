de085ab6394ffed24b33ff7ac6f42552cdebd9da
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2020-04-21 18:46:42 +0300

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
-  const result = useQuery(ALL_BOOKS,{
+  const [genre, setGenre] = useState('')
+  const result = useQuery(ALL_BOOKS, {
     pollInterval: 2000
   })
 
-  if (result.loading)  {
+  if (result.loading) {
     return <div>loading...</div>
   }
-  
+
   if (!props.show) {
     return null
   }
   console.log(result.data)
   const books = result.data.allBooks
+
+  const genres = new Set()
+  books.map(b => b.genres).forEach(list => {
+    list.forEach(genre => {
+      genres.add(genre)
+    })
+  })
+
 
   return (
     <div>
@@ -32,15 +41,30 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
+          {genre ?
+            books.filter(b => b.genres.includes(genre)).map(a =>
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )
+            :
+            books.map(a =>
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )}
         </tbody>
       </table>
+      <div>
+        {Array.from(genres).map(g =>
+          <button onClick={() => setGenre(g)}>{g}</button>)
+        }
+        <button onClick={() => setGenre('')}>all books</button>
+      </div>
     </div>
   )
 }
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -6,6 +6,7 @@ export const ALL_BOOKS = gql`
       title
       author {name}
       published
+      genres
       id
     }
   }
